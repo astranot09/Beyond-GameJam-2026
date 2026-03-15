46e53f6713061314d96bbbacb60aea3de8905a07
Stopit-m8 <sunelsensurya@gmail.com>
2026-03-15 15:56:03 +0700

    jndajkdnakndankds

--- Beyond GameJam/Assets/Scripts/UI/UIManager.cs
+++ Beyond GameJam/Assets/Scripts/UI/UIManager.cs
@@ -5,9 +5,22 @@ public class UIManager : MonoBehaviour
     public GameObject creditPanel;
     public GameObject optionPanel;
 
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
     public void optionPanelUI()
     {
         optionPanel.SetActive(!optionPanel.activeSelf);
+        if (optionPanel.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+        
     }
     public void creditPanelUI()
     {
